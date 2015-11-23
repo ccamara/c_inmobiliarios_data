5c0e9939e5b5566001cf0f1fe4bd7ceac26004cb
Carlos Cámara <carlos.camara@gmail.com>
2015-11-24 00:18:58 +0100

    Updated script to read all ws and combine them all into one.

--- cadaveres.adoptados.R
+++ cadaveres.adoptados.R
@@ -14,28 +14,30 @@ require(tidyr)
 # Loading 'Clon de Adopta un cadáver' spreadsheet using its key.
 ss = gs_key("1PvvaBcSyQP_0SYo2epQcPAU47rA--E_6KSCdMKSQKcg")
 
-data_andalucia = ss %>%
-  gs_read(ws = 1, range = cell_cols(1:86))
+ss$n_ws # Number of worksheets.
 
-data_aragon = ss %>%
-  gs_read(ws = 2, range = cell_cols(1:86))
+# Load all worksheets and store them in data frames.
+for (i in seq_along(ss$n_ws)) {
+  data_temp = paste("data", i, sep = "")
+  assign(data_temp, gs_read(ss, ws = ss_ws[i], range = cell_cols(1:86)))
+}
 
-
-# Combining all sheets in a single data frame -----------------------------
-
-data = rbind(data_andalucia,
-             data_aragon)
+# Combine all worksheets into a single data frame.
+data = rbind(data1, data2, data3, data4, data5, data6, data7,
+             data8, data9, data10, data11, data12, data13, data14, data15,
+             data16, data17, data18, data19)
 
 # Data manipulation -------------------------------------------------------
 
-# Filter wrong format columns.
-
-# Split latitude and latitude column into separate variables.
+# Filtering out unusable rows and split latitude and latitude column into
+# separate variables.
 data = data %>%
+  filter(complete.cases(nombre_promocional)) %>%
   filter(complete.cases(latitud_longitud)) %>%
   separate(latitud_longitud,c("latitud","longitud"), ",")
 
+# @todo: turn - characters into NA characters.
 
 # Export file again -------------------------------------------------------
 
-write.csv(data, file = "data/cadaveres.adoptados.csv", na = "")
+write.csv(data, file = "data/cadaveres.adoptados.csv", na = c("", "-", NA))
