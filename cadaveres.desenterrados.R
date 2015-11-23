11074e2a9979d0401912397efe921db410e1b84b
Carlos Cámara <carlos.camara@gmail.com>
2015-11-23 19:48:20 +0100

    Created first parser for cadaveres desenterrados.

---
+++ cadaveres.desenterrados.R
@@ -0,0 +1,27 @@
+# This script downloads and cleans data to be imported in cadáveres
+# inmobiliarios' database.
+
+
+# Packages and dependencies -----------------------------------------------
+
+# Load required packages and install them if they are not available.
+require(googlesheets)
+suppressMessages(require(dplyr))
+require(tidyr)
+
+# Data import -------------------------------------------------------------
+
+# Loading 'Clon de cadáveres desenterrados' spreadsheet using its key.
+ss = gs_key("1cV4K6ZihxRAzwlae1Pp6kIlW-o9JpmIDRpy98cQQk6I")
+
+
+# Nación rotonda cleanup and manipulations --------------------------------
+
+data_nacion_rotonda = ss %>%
+  gs_read(ws = 1, range = cell_cols(2:86)) %>%
+  filter(complete.cases(latitud_longitud)) %>%
+  separate(latitud_longitud,c("latitud","longitud"), ",")
+
+write.csv(data_nacion_rotonda,
+          file = "data/cadaveres.desenterrados.nacion-rotonda.csv",
+          na = "")
