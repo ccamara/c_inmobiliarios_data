e79674734fc882dd0e4f443b0b6e41f7e301433e
Carlos Cámara <carlos.camara@gmail.com>
2016-02-22 16:31:05 +0100

    Nacion rotonda's data processing.

--- cadaveres.desenterrados.R
+++ cadaveres.desenterrados.R
@@ -9,23 +9,56 @@ require(googlesheets)
 suppressMessages(require(dplyr))
 require(tidyr)
 
-# Data import -------------------------------------------------------------
+
+# Ruinas modernas ---------------------------------------------------------
+
+# Data import
 
 # Loading 'Clon de cadáveres desenterrados' spreadsheet using its key.
 ss = gs_key("1cV4K6ZihxRAzwlae1Pp6kIlW-o9JpmIDRpy98cQQk6I")
 
 
-# Nación rotonda cleanup and manipulations --------------------------------
+# Cleanup and manipulations
 
-data_nacion_rotonda = ss %>%
+data_ruinas_modernas = ss %>%
   gs_read(ws = 1, range = cell_cols(2:86)) %>%
   filter(complete.cases(latitud_longitud)) %>%
   separate(latitud_longitud,c("latitud","longitud"), ",")
 
 # Adding new information for future classification on the website.
 
+data_ruinas_modernas$corpse_category = "Desenterrado"
+
+write.csv(data_ruinas_modernas,
+          file = "data/cadaveres.desenterrados.ruinas_modernas.csv",
+          na = "")
+
+
+# Nación rotonda ----------------------------------------------------------
+
+data_nacion_rotonda = read.csv(
+  'data/input/BBDD cadáveres inmobiliarios Nación Rotonda.csv')
+
+# Cleanup and manipulations
+data_nacion_rotonda$Location = gsub('<Point><coordinates>|</coordinates></Point>',
+                                    '',
+                                    data_nacion_rotonda$Location)
+
+data_nacion_rotonda$Location = gsub(',0.0',
+                                    '',
+                                    data_nacion_rotonda$Location)
+
+data_nacion_rotonda = data_nacion_rotonda %>%
+  rename(nombre_promocional = Name) %>%
+  rename(enlace_web = Haz.Click.Aquí) %>%
+  filter(complete.cases(nombre_promocional)) %>%
+  separate(Location,c('longitud','latitud'), ",")
+
+# Adding new information for future classification on the website.
+
 data_nacion_rotonda$corpse_category = "Desenterrado"
 
 write.csv(data_nacion_rotonda,
-          file = "data/cadaveres.desenterrados.nacion-rotonda.csv",
+          file = "data/cadaveres.desenterrados.nacion_rotonda.csv",
           na = "")
+
